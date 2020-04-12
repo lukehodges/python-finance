c29e121b03ea63f4e5e0aef91d2aef3905ec6a04
luke hodges <58270858+lukehodges@users.noreply.github.com>
2020-04-12 11:21:49 +0100

    Create interest.py

---
+++ interest.py
@@ -0,0 +1,11 @@
+class day:
+  def simple(principal,interest,days):
+    return principal * (1+interest*days/365)
+   def compound(principal,interest,days):
+    return prinipal * (1+interest)**(days/365)
+class years:
+  def simple(principal,interest,years):
+    return prinipal * (1+interest*years)
+  def compound(principal,interest,years:
+    return prinipal * (1+interest)**years
+    
